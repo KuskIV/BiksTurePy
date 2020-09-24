663ed8ff95dc60bfbfeb60a15d7ad906d06d316a
Jeppe <jeppe@bandsholm.com>
2020-09-24 13:36:51 +0200

    ups another push

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ import math
 
 
 #DATASET_PATH = 'C:\\Users\\jeppe\\Desktop\\GTSRB_Final_Training_Images\\GTSRB\\Final_Training\\Images' # assume it is in root
-DATASET_PATH = ''
+DATASET_PATH = 'BiksBois\\images\\GTSRB_Final_Training_Images\\GTSRB\\Final_Training\\Images'
 #DATASET_PATH = 'C:\\Users\\jeppe\\Desktop\\FullIJCNN2013'
 #DATASET_PATH = 'FullIJCNN2013'
 
