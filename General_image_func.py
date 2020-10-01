34f96ec78e4024ca3f89a340125133a4148627e8
Jeppe <jeppe@bandsholm.com>
2020-10-01 12:09:54 +0200

    small refagtoring of img funcs, and hello world

--- General_image_func.py
+++ General_image_func.py
@@ -12,8 +12,7 @@ def changeImageSize(maxWidth: int,
     newWidth    = int(widthRatio*image.size[0])
     newHeight   = int(heightRatio*image.size[1])
 
-    newImage    = image.resize((newWidth, newHeight))
-    return newImage
+    return image.resize((newWidth, newHeight))
 
 def EnsureUniformImageShape(img1: Image.Image,img2: Image.Image, shape=None):
     size1 = img1.size
@@ -29,8 +28,6 @@ def EnsureUniformImageShape(img1: Image.Image,img2: Image.Image, shape=None):
         img2 = changeImageSize(size1[0],size1[1],img2)
         return img1,img2
 
-
-
 def merge_two_images(img1: Image.Image
                     ,img2: Image.Image,
                      alpha:float=0.25, 
@@ -40,11 +37,12 @@ def merge_two_images(img1: Image.Image
     image5 = image3.convert("RGBA")
     image6 = image4.convert("RGBA")
 
-    alphaBlended1 = Image.blend(image5, image6, alpha=alpha)
+    return Image.blend(image5, image6, alpha=alpha)
 
-    return alphaBlended1
-def convertToPILImg(img1: np.array)-> Image.Image:
-    numpy_image_rescaled = img1 * 255
+def convertToPILImg(img1: np.array, normilized = True)-> Image.Image:
+
+    if normilized is True:
+        numpy_image_rescaled = img1 * 255
     # converting the dfloat64 numpy to a unit8 - is required by PIL
     numpy_image_rescaled_uint8 = np.array(numpy_image_rescaled, np.uint8)
     # convert to PIL and show
