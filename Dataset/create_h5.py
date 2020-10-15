53921a4b8cdadb59358969053580af3a262cfb6f
GreatestHoro <rhoro18@student.aau.dk>
2020-10-15 16:47:52 +0200

    i did it

--- Dataset/create_h5.py
+++ Dataset/create_h5.py
@@ -5,32 +5,28 @@ import io
 from PIL import Image
 import os.path
 from os import path
-
-import os,sys,inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir) 
-
-from progressbar import print_progressbar
+from tqdm import tqdm
+from tqdm import trange
 
 def generate_h5(h5Path, dataset_path):
 
-    print(f"A new H5PY file is about to be created.\n    Dataset path: {dataset_path}\nH5PY Path: {h5Path}")
+    print(f"\n\n\nA new H5PY file is about to be created.\n    Dataset path: {dataset_path}\n    H5PY Path: {h5Path}\n\n")
     if path.exists(h5Path):
         print("The H5PY file has been located, and is being overwritten")
         h5 = h5py.File(h5Path, 'w')
     else:
         h5 = h5py.File(h5Path, 'a')
         print("The H5PY file could not be found at the path, and a new is created")
-
-    print_progressbar(0, 43, prefix = 'Progress:', suffix = 'Complete', length = 50)
-    for i in os.listdir(dataset_path):
-        group_name = os.path.join(dataset_path, i)
+    
+    done = len(os.listdir(dataset_path))
+    classes = trange(len(os.listdir(dataset_path)), desc='Class stuff', leave=True)
+    for i in classes:
+        classes.set_description(f"Class {i + 1} / {done}")
+        classes.refresh()
+        
+        group_name = os.path.join(dataset_path, os.listdir(dataset_path)[i])
 
         group = h5.create_group(group_name)
-        print("Group: ", i)
-        print_progressbar(int(i) + 1, 43, prefix = 'Progress:', suffix = 'Complete', length = 50)
-
 
         for j in os.listdir(group_name):
             img_path = os.path.join(group_name, j)
