03b6b58295b9039d185964b6cb97008499fdf935
KuskIV <madshkusk@gmail.com>
2020-10-15 12:50:51 +0200

    import fix

--- phase_one/find_ideal_model.py
+++ phase_one/find_ideal_model.py
@@ -10,7 +10,6 @@ current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
 
-from data import get_data, split_data  # Not an error
 from general_image_func import get_class_names                          # Not an error
 from Models.create_model import flatten_and_dense          # Not an error
 
