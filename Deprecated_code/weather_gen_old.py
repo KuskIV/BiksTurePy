4ca91c169315febafa04feeebdd2e448a916c93b
Jeppe <jeppe@bandsholm.com>
2020-10-24 15:07:13 +0200

    constructor for the weahter class

--- Deprecated_code/weather_gen_old.py
+++ Deprecated_code/weather_gen_old.py
@@ -3,42 +3,7 @@ from PIL import Image
 import numpy as np
 import imgaug as ia
 from imgaug import augmenters as iaa
-import os,sys,inspect
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir) 
 import general_image_func as gif 
-
-class weather:
-
-    #default values
-    density = (0.03,0.14)
-    density_uniformity = (0.8,1.0)
-    drop_size = (0.3,0.4)
-    drop_size_uniformity = (0.1,0.5)
-    angle = (-15,15)
-    speed=(0.1,0.2)
-    blur=(0.001, 0.001)
-
-    def __init__(self, config:dict):
-
-        Keys = ['density','density_uniformity','drop_size','drop_size_uniformity','angle','speed','blur']
-        if Keys[0] in config:
-            self.density = config.get(Keys[0])
-        if config.get(Keys[1]) != None:
-            self.density_uniformity = config.get(Keys[1])
-        if config.get(Keys[2]) != None:
-            self.drop_size = config.get(Keys[2])
-        if config.get(Keys[3]) != None:
-            self.drop_size_uniformity = config.get(Keys[3])
-        if config.get(Keys[4]) != None:
-            self.angle = config.get(Keys[4])
-        if config.get(Keys[5]) != None:
-            self.speed = config.get(Keys[5])
-        if config.get(Keys[6]) != None:
-            self.blur = config.get(Keys[6])
-
-
 
 def rain(image:Image.Image, density:tuple=(0.03,0.14), density_uniformity:tuple=(0.8,1.0), drop_size:tuple=(0.3,0.4), drop_size_uniformity:tuple=(0.1,0.5),angle:tuple=(-15,15), speed:tuple=(0.1,0.2), blur:tuple=(0.001, 0.001))->Image.Image: 
     """
@@ -127,11 +92,67 @@ def snow(image:Image.Image, density:tuple=(0.03,0.14), density_uniformity:tuple=
     pil_image = Image.fromarray(img_aug.astype('uint8'), 'RGB')
     return pil_image
 
+def fog(image:Image.Image, intensity:tuple=(190,255), intensity_freq_exponent:tuple=(-2.5, -1.5), intensity_coarse_scale:tuple=(0,10), alpha_min:tuple=(0.1), alpha_multiplier:tuple=(0.3, 1.0), alpha_size_px_max:tuple=(1), alpha_freq_exponent:tuple=(-4.0,-1.5), sparsity:float=(1.0), density_multiplier:tuple=(0.5,1.5))->Image.Image:
+    """[Method to add fog to images]
+    #TODO: fine tune params
+    Args:
+        image (Image.Image): [description]
+        intensity (tuple, optional): [description]. Defaults to (190,255).
+        intensity_freq_exponent (tuple, optional): [description]. Defaults to (-2.5, -1.5).
+        intensity_coarse_scale (tuple, optional): [description]. Defaults to (0,10).
+        alpha_min (tuple, optional): [description]. Defaults to (0.1).
+        alpha_multiplier (tuple, optional): [description]. Defaults to (0.3, 1.0).
+        alpha_size_px_max (tuple, optional): [description]. Defaults to (1).
+        alpha_freq_exponent (tuple, optional): [description]. Defaults to (-4.0,-1.5).
+        sparsity (float, optional): [description]. Defaults to (1.0).
+        density_multiplier (tuple, optional): [description]. Defaults to (0.5,1.5).
+
+    Returns:
+        Image.Image: [description]
+    """
+    noise_layer = iaa.CloudLayer(
+        intensity_mean=intensity,
+        intensity_freq_exponent = intensity_freq_exponent,
+        intensity_coarse_scale = intensity_coarse_scale,
+        alpha_min= alpha_min,
+        alpha_multiplier= alpha_multiplier,
+        alpha_size_px_max= alpha_size_px_max,
+        alpha_freq_exponent= alpha_freq_exponent,
+        sparsity=sparsity,
+        density_multiplier=density_multiplier
+    )
     images = np.asarray(image, dtype="uint8")
     img_aug = noise_layer.augment_image(images)
 
     pil_image = Image.fromarray(img_aug.astype('uint8'), 'RGB')
     return pil_image
+
+
+def snow_on(image:Image.Image, ligtness_multiplier:float=(1.0,4.0), ligtness_threshold:float=(100,255))-> Image.Image:
+    """Adds snow on lanscape images, NOTE this dosnt work well with other types of images 
+
+    Args:
+        image (Image.Image): [description]
+        ligtness_multiplier (float, optional): [description]. Defaults to (1.0,4.0).
+        ligtness_threshold (float, optional): [description]. Defaults to (100,255).
+
+    Returns:
+        Image.Image: [description]
+    """
+    noise_layer = iaa.FastSnowyLandscape(
+        lightness_multiplier=ligtness_multiplier,
+        lightness_threshold=ligtness_threshold
+
+    )
+    images = np.asarray(image, dtype="uint8")
+    img_aug = noise_layer.augment_image(images)
+
+    pil_image = Image.fromarray(img_aug.astype('uint8'), 'RGB')
+    return pil_image
+
+
+
+
 
 def QuickDebug()-> None:
     # img = Image.open('Dataset/images/00000/00000_00029.ppm')
@@ -139,12 +160,11 @@ def QuickDebug()-> None:
     # img = img.save(f'C:/Users/roni/Desktop/rain/1.png')
     # n = [0.1, 0.3, 0.6, 1]
     for i in range(100):
-        img = Image.open('C:/Users/jeppe/Desktop/FullIJCNN2013/00/00000.ppm')
-        
+        img = Image.open('Dataset/images/00000/00000_00029.ppm')
+        # img.show()
     
-        img = snow(img)
-        img.show()
-        #img.save(f'C:/Users/roni/Desktop/rain/{i}.png')
+        img = fog(img)
+        img.save(f'C:/Users/roni/Desktop/rain/{i}.png')
     # for i in range(0,50):
     #     img = Image.open('Dataset/images/00000/00000_00029.ppm')
     #     # img.show()
