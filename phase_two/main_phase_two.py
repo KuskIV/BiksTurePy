ed4b7226916364c7894c30035ea49f154a9fa132
GreatestHoro <rhoro18@student.aau.dk>
2020-10-26 15:14:42 +0100

    minor fixes

--- phase_two/main_phase_two.py
+++ phase_two/main_phase_two.py
@@ -11,7 +11,7 @@ import numpy as np
 
 def phase_2_1(model, h5path, lazysplit, image_size):
     h5 = h5_object(h5path, training_split=dataset_split)
-    values = []
+    values = [("image","class","filter","predicted_class")]
     for j in range(lazy_split):
         original_images, original_labels, _, _ = h5_obj.shuffle_and_lazyload(j, lazy_split) #TODO need variant of this that does not generate test set or shuffle
         image_tuples = add_noise((original_images,original_labels)) #tuple(image,class,filter)
@@ -20,7 +20,7 @@ def phase_2_1(model, h5path, lazysplit, image_size):
             predicted_label = np.argmax(prediction) #Get the class with highest liklyhood of the predictions
             image_tuples[i] = image_tuples[i]+(predicted_label) #concatanate two tuples to create new tuple , which replacess the old one
         values.extend(image_tuples)
-    convert_to_csv('phase_two/phase2_results.csv',headers,values) #tuple(image,class,filter,predicted_class)
+    convert_to_csv('phase_two/phase2_results.csv',values) #tuple(image,class,filter,predicted_class)
 
 def load_filters():
     F = premade_single_filter('fog')
@@ -34,10 +34,9 @@ def load_filters():
 def add_noise(imgs):
     return apply_multiple_filters(imgs,filters = load_filters(), mode='rand', KeepOriginal=True)
 
-def convert_to_csv(path,headers,values):
+def convert_to_csv(path,values):
     with open(path, mode='w') as phase2_results:
         phase2_writer = csv.writer(phase2_results, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        phase2_writer.writerow(list(headers))
         
         for value in values:
             phase2_writer.writerow(list(value))
@@ -61,7 +60,7 @@ def group_by_feature(header,csv_reader,feature_lable:str):
     return groups
 
 def create_csv_to_plot():
-    newdatapoint = []
+    newdatapoint = [('class','filters','error')]
     with open('phase_two/phase2_results.csv', 'r') as read_obj:
         csv_reader = reader(read_obj)
         header = next(csv_reader)
@@ -71,7 +70,7 @@ def create_csv_to_plot():
             for _class in classes:
                 error = calculate_error(_class)
                 newdatapoint.append((_class,group,error)) #(class,filter,error)
-            convert_to_csv(f'phase_two/phase2_{group}.csv', ['class','filters','error'], newdatapoint)
+            convert_to_csv(f'phase_two/phase2_{group}.csv', newdatapoint)
 
 def autolabel(rects):
     """Attach a text label above each bar in *rects*, displaying its height."""
@@ -100,7 +99,7 @@ def plot_phase2_result(path):
     fig, ax = plt.subplots()
     
     rects = []
-    for i in range(len(_classes))
+    for i in range(len(_classes)):
         rects.append(ax.bar(x - width/2, errors[i], width, label=_classes[i]))
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Errors')
@@ -124,11 +123,5 @@ def plot_phase2_results():
     
 def QuickDebug():
 
-    large_model_path, medium_model_path, small_model_path,belgium_model_path = get_test_model_paths()
-        
-    #large_model = tf.keras.models.load_model(large_model_path)
-    #medium_model = tf.keras.models.load_model(medium_model_path)
-    # small_model = tf.keras.models.load_model(small_model_path)
-    belgium_model = tf.keras.models.load_model(belgium_model_path)
-    phase_2_1(belgium_model,,10,(32,32))
+    # phase_2_1(belgium_model,,10,(32,32))
 
