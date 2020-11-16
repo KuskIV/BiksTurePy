748b1c296e5a3dc185d4c8d2a79354a709f8014f
KuskIV <madshkusk@gmail.com>
2020-11-17 00:25:25 +0100

    Getting closer to 100% every day

--- Unit_tests/test_sum_sum.py
+++ Unit_tests/test_sum_sum.py
@@ -10,6 +10,7 @@ from plot.write_csv_file import cvs_object
 import plot.sum_for_model as sum_method
 from phase_one.experiment_one import sum_plot, sum_summed_plots
 from phase_one.find_ideal_model import get_satina_gains_model_object_list
+from plot.sum_for_model import sum_for_model, sum_for_class_accuracy, sum_summed_for_class_accuracy
 
 class Test_sum_sum(unittest.TestCase):
     def test_merge_final_files(self):
@@ -27,11 +28,11 @@ class Test_sum_sum(unittest.TestCase):
         self.assertEqual(expected_rows, output_lines)
     
     def test_sum_plot(self):
-        base_path = 'Unit_tests/csv_test_data/sum_plot'
-        expected_csv_path = f"{base_path}/result_model_24_test.csv"
-        output_csv_path = f"{base_path}/model_24_test.csv"
+        base_path = 'Unit_tests/csv_test_data/sum_plot_csv'
+        expected_csv_path = f"{base_path}/result_model_24_test_summed.csv"
+        output_csv_path = f"{base_path}/model_24_test_summed.csv"
         
-        model_object_list = [get_satina_gains_model_object_list()[-1]]
+        model_object_list = [get_satina_gains_model_object_list(120)[-1]]
         
         sum_plot(model_object_list, 'test', base_path)
         
@@ -43,15 +44,51 @@ class Test_sum_sum(unittest.TestCase):
 
         self.assertEqual(expected_rows, output_rows)
     
-    def test_sum_summed_plot(self):
-        base_path = 'Unit_tests/csv_test_data/sum_plot'
-        expected_csv_path = f"{base_path}/result_model_24_test_summed.csv"
-        output_csv_path = f"{base_path}/model_24_test_summed.csv"
+    def test_sum_summed_plot(self): #TODO Adjust CSV
+        base_path = 'Unit_tests/csv_test_data/sum_summed_plot_csv'
+        expected_csv_path = f"{base_path}/result_test_sum_summed.csv"
+        output_csv_path = f"{base_path}/test_sum_summed.csv"
 
-        model_object_list = get_satina_gains_model_object_list()
+        model_object_list = get_satina_gains_model_object_list(120)
 
         sum_summed_plots(model_object_list, 'test', base_path)
 
+        expected_csv = cvs_object(expected_csv_path)
+        output_csv = cvs_object(output_csv_path)
+
+        expected_rows = expected_csv.get_lines()
+        output_rows = output_csv.get_lines()
+
+        self.assertEqual(expected_rows, output_rows)
+    
+    def test_sum_summed_for_class_accuracy(self): #TODO Adjust CSV
+        base_path = 'Unit_tests/csv_test_data/sum_summed_for_class_accuracy_csv'
+        expected_csv_path = f"{base_path}/result_test_sum_summed_class_accuracy.csv"
+        output_csv_path = f"{base_path}/test_sum_summed_class_accuracy.csv"
+        input_csv_path = f"{base_path}/test_sum_class_accuracy.csv"
+
+        csv_obj = cvs_object(input_csv_path)
+        data = sum_summed_for_class_accuracy(csv_obj)
+        csv_obj.write(data, path=output_csv_path, overwrite_path=True)
+
+        expected_csv = cvs_object(expected_csv_path)
+        output_csv = cvs_object(output_csv_path)
+
+        expected_rows = expected_csv.get_lines()
+        output_rows = output_csv.get_lines()
+
+        self.assertEqual(expected_rows, output_rows)
+        
+    def test_sum_for_class_accuracy(self): #TODO Adjust CSV
+        base_path = 'Unit_tests/csv_test_data/sum_for_class_accuracy_csv'
+        expected_csv_path = f"{base_path}/result_test_sum_class_accuracy.csv"
+        output_csv_path = f"{base_path}/test_sum_class_accuracy.csv"
+        input_csv_path = f"{base_path}/test_class_accuracy.csv"
+        
+        data_class_acc_val = sum_for_class_accuracy(cvs_object(input_csv_path))
+        csv_obj = cvs_object(output_csv_path)
+        csv_obj.write(data_class_acc_val)
+        
         expected_csv = cvs_object(expected_csv_path)
         output_csv = cvs_object(output_csv_path)
 
